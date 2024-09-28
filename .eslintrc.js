46030401d0cecef7a49d708cc72e6ac7c2e92525
Richard Neves <ricknevesbc@icloud.com>
2024-09-28 17:08:05 -0300

    Initial Project

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['eslint-plugin-import-helpers'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-useless-empty-export': 'off',
+    'no-useless-constructor': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          ['/^nest/', 'module', '/^@nestjs/'],
+          ['/^@/'],
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+}
